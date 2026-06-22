da9deaf35d5a02ee4bbd57fa0c12453ebaf9c4b0
Asamunaicha013 <ab01301230@gmail.com>
2026-06-22 20:35:17 +0800

    Rename 星號繪三角形.c to 星號繪三角形.cpp

---
+++ 星號繪三角形.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int mode;
+    cin >> mode;
+
+    if (mode == 1) {
+        cout << "    *\n";
+        cout << "   * *\n";
+        cout << "  * *\n";
+        cout << " * *\n";
+        cout << "*********\n";
+    } else if (mode == 2) {
+        cout << "    *\n";
+        cout << "   ***\n";
+        cout << "  *****\n";
+        cout << " *******\n";
+        cout << "*********\n";
+    } else if (mode == 3) {
+        cout << "*********\n";
+        cout << " *******\n";
+        cout << "  *****\n";
+        cout << "   ***\n";
+        cout << "    *\n";
+    }
+
+    return 0;
+}
+
+    return 0;
+}
