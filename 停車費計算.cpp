13842d762e37b19e345462280762b9f853ad0e1b
Asamunaicha013 <ab01301230@gmail.com>
2026-06-22 20:26:17 +0800

    Update and rename 停車費計算.c to 停車費計算.cpp

--- 停車費計算.cpp
+++ 停車費計算.cpp
@@ -1,30 +1,53 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(){
+using namespace std;
+
+int main() {
     int hour, minute, hour2, minute2, sum;
     int total = 0;
     int temp = 0;
-    scanf("%d %d", &hour, &minute);
-    scanf("%d %d", &hour2, &minute2);
-    if(minute > minute2){
-        hour2 -= 1 ;
+    cin >> hour >> minute;
+    cin >> hour2 >> minute2;
+    
+    if(minute > minute2) {
+        hour2 -= 1;
         minute2 += 60;
         sum = (hour2 - hour) * 60 + (minute2 - minute);
-        while((sum - temp) >= 30 ){
-            if(temp < 120){
+        while((sum - temp) >= 30) {
+            if(temp < 120) {
                 total += 30;
                 temp += 30;
             }
-            else if(temp >= 120 && temp < 240){
+            else if(temp >= 120 && temp < 240) {
                 total += 40;
                 temp += 30;
             }
-            else if(temp >= 240){
+            else if(temp >= 240) {
                 total += 60;
                 temp += 30;
             }
         }
     }
+    else {
+        sum = (hour2 - hour) * 60 + (minute2 - minute);
+        while((sum - temp) >= 30) {
+            if(temp < 120) {
+                total += 30;
+                temp += 30;
+            }
+            else if(temp >= 120 && temp < 240) {
+                total += 40;
+                temp += 30;
+            }
+            else if(temp >= 240) {
+                total += 60;
+                temp += 30;
+            }
+        }
+    }
+    cout << total;
+    return 0;
+}
 
     else{
         sum = (hour2 - hour) * 60 + (minute2 - minute);
