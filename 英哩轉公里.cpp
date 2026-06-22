cb7f250edca5aecccaf92f3405266075c9133205
Asamunaicha013 <ab01301230@gmail.com>
2026-06-22 20:43:44 +0800

    Refactor code by cleaning up comments
    Removed unnecessary comment about controlling decimal places.

--- 英哩轉公里.cpp
+++ 英哩轉公里.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <iomanip> // 引入 iomanip 以控制小數位數
+#include <iomanip> 
 
 using namespace std;
 
@@ -8,8 +8,6 @@ int main() {
     cin >> kilm;
     
     double k = kilm * 1.6;
-    
-    // 使用 fixed 和 setprecision(1) 來達到 printf("km=%.1f") 的效果
     cout << "km=" << fixed << setprecision(1) << k << "\n";
     
     return 0;
